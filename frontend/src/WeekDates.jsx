df40c04b46e51cdaa3b2d896b5809d19673af79e
Prashant Kumar Singh <prashantsingh090798@gmail.com>
2024-04-04 01:22:26 +0530

    fixed:filled work hours are persistent across date shifts

--- frontend/src/WeekDates.jsx
+++ frontend/src/WeekDates.jsx
@@ -20,8 +20,6 @@ function WeekDates({
 
     const [weekDateDivs, setWeekDateDivs] = useState([])
 
-    const weekdaysNumericRep = ['Sun', 'Mon', 'Tue', 'Wed', 'Thru', 'Fri', 'Sat']
-
     useEffect(() => {
         createweekDateDivsOfSelectedDate()
     }, [year,
@@ -29,13 +27,15 @@ function WeekDates({
         weekStartDay,
         weekEndDay,])
 
+    const weekdaysNumericRep = ['Sun', 'Mon', 'Tue', 'Wed', 'Thru', 'Fri', 'Sat']
+
     const createweekDateDivsOfSelectedDate = useCallback(() => {
         let weekDateDivsTemp = []
         let dayIndex = startDayIndex
         for (let weekDay = weekStartDay; weekDay <= weekEndDay; weekDay++) {
-            console.log('Day = ' + weekdaysNumericRep[dayIndex])
             const weekDate = year + '-' + (month.toString().length === 1 ? '0' + month : month) + '-' + (weekDay.toString().length === 1 ? '0' + weekDay : weekDay)
-            console.log('weekDate = ' + weekDate)
+            // console.log('weekDate = ' + weekDate)
+            // console.log('Day = ' + weekdaysNumericRep[dayIndex])
             weekDateDivsTemp.push((
                 <div key={"keyDiv" + weekDate}>
                     Week-Date {' '} {weekDate} {' '} {weekdaysNumericRep[dayIndex]}
@@ -46,10 +46,10 @@ function WeekDates({
                         key={"keyInputHours" + weekDate}
                         type="number"
                         name={"labelHours" + weekDate}
-                        value = {!(attendanceDates[weekDate] === undefined) ? attendanceDates[weekDate] : ''}
+                        value={attendanceDates[weekDate]}
                         onChange={(e) => {
-                            console.log(weekDate)
-                            console.log(e.target.value)
+                            // console.log(weekDate)
+                            // console.log(e.target.value)
                             if (e.target.value >= 1) {
                                 const attendanceDatesTemp = attendanceDates
                                 attendanceDatesTemp[weekDate] = Number(e.target.value)
@@ -64,27 +64,6 @@ function WeekDates({
                             console.log(attendanceDates)
                         }} />
                     {' '}
-                    {/* <label key={"keyLabelTick" + weekDate} htmlFor={"labelTick" + weekDate}>Leave = Zero Work Hour</label>
-                    {' '}
-                    <input
-                        key={"keyInputTick" + weekDate}
-                        type="checkbox"
-                        name={"labelTick" + weekDate}
-                        onChange={(e) => {
-
-                        }} /> */}
-                    {/* <button
-                        key={"keyInputTick" + weekDate}
-                        name={"labelTick" + weekDate}
-                        onClick={() => {
-                            const attendanceDatesTemp = attendanceDates
-                            attendanceDatesTemp[weekDate] = '0'
-                            onAttendanceDatesChange(attendanceDatesTemp)
-                            console.log(weekDate)
-                            console.log(attendanceDates)
-                        }}>
-                        Click if you were on Leave on this Day
-                    </button> */}
                 </div>
             ))
             dayIndex = (++dayIndex) % 7
@@ -104,4 +83,28 @@ if (!(!!attendanceDates.weekDay || attendanceDates.weekDay))
 if (attendanceDates.weekDay)
     attendanceDatesTemp.weekDay = 0
 setAttendanceDates(attendanceDatesTemp) 
+*/
+
+/*
+<label key={"keyLabelTick" + weekDate} htmlFor={"labelTick" + weekDate}>Leave = Zero Work Hour</label>
+{' '}
+<input
+    key={"keyInputTick" + weekDate}
+    type="checkbox"
+    name={"labelTick" + weekDate}
+    onChange={(e) => {
+
+    }} /> 
+<button
+key={"keyInputTick" + weekDate}
+name={"labelTick" + weekDate}
+onClick={() => {
+    const attendanceDatesTemp = attendanceDates
+    attendanceDatesTemp[weekDate] = '0'
+    onAttendanceDatesChange(attendanceDatesTemp)
+    console.log(weekDate)
+    console.log(attendanceDates)
+}}>
+Click if you were on Leave on this Day
+</button> 
 */
